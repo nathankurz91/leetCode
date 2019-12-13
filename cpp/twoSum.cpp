0ab0d1d9e58964c38be4b72053fa17bfc522c8e4
nathankurz <nathankurz91@gmail.com>
2019-12-12 21:31:02 -0500

    initial leetcode commit

---
+++ cpp/twoSum.cpp
@@ -0,0 +1,39 @@
+// Developer: Nathan Kurz
+// twoSum.cpp
+// Description: Checks an array of integers to determine if there are two specific values that add
+//              up to the target value
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector <int>nums;
+    nums.push_back(1);
+    nums.push_back(3);
+    nums.push_back(5);
+    nums.push_back(7);
+    int target = 8;
+    vector <int>result;
+    result = twoSum(nums, target);
+    cout<<result[0];
+    return 0;
+}
+
+vector <int>twoSum(vector<int>& nums, int target) {
+    vector<int> result;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        for (int j = i + 1; j < nums.size(); j++)
+        {
+            if (nums[i] + nums[j] == target)
+            {
+                result.push_back(i);
+                result.push_back(j);
+            }
+        }
+    }
+    return result;
+}
