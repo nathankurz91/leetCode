e98cbe9a2672370ccbfb9737fa049b6374e0b872
nathankurz <nathankurz91@gmail.com>
2019-12-13 19:25:43 -0500

    toLowerCase

---
+++ js/toLowerCase.js
@@ -0,0 +1,9 @@
+/**
+ * This is sort of strange to ask this question in javascript because there is a method with literally the exact 
+ * same signature in javascript already
+ * @param {string} str
+ * @return {string}
+ */
+var toLowerCase = function(str) {
+    return str.toLowerCase();
+};
